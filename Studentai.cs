b07edfd50eccd1be80806c8cd8096a327b2280de
Valdemar Kozenevski <valdemar.kozenevski@stud.vgtu.lt>
2020-03-18 20:43:26 +0200

    studentu klases

---
+++ Studentai.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPAprograma
+{
+    class Stud 
+    {
+        public string v;
+        public string p;
+        public double egz;
+
+        List<double> pzm;
+
+        public Stud(string vardas, string pavarde)
+        {
+            v = vardas;
+            p = pavarde;
+            pzm = new List<double>();
+        }
+
+        public Stud(string vardas, string pavarde, List<double> grades)
+        {
+            pzm = grades;
+        }
+
+        public Stud(string vardas, string pavarde, List<double> grades, double egzGrade) : this(vardas, pavarde, grades)
+        {
+            egz = egzGrade;
+        }       
+    }
+
+
+    class Studentai
+    {
+
+
+        public Studentai()
+        {
+
+        }
+
+    }
+}
