cc1c892e39d89cd034cb2a49833fd86a477f0f85
Valdemar Kozenevski <valdemar.kozenevski@stud.vgtu.lt>
2020-03-18 20:52:07 +0200

    mean and median calc

--- Studentai.cs
+++ Studentai.cs
@@ -29,15 +29,49 @@ namespace IPAprograma
         public Stud(string vardas, string pavarde, List<double> grades, double egzGrade) : this(vardas, pavarde, grades)
         {
             egz = egzGrade;
-        }       
+        }
+
+        public double GetMean()
+        {
+            if(pzm.Count == 0)
+            {
+                return 0;
+            }
+            return (double)pzm.Sum() / (double)pzm.Count;
+        }
+
+        public double GetMedian()
+        {
+            double[] temp = pzm.ToArray();
+            Array.Sort(temp);
+            int count = temp.Length;
+            if (count == 0)
+            {
+                return 0;
+            }
+            else if (count % 2 == 0)
+            {                
+                return (temp[count / 2 - 1] + temp[count / 2]) / 2.0;
+            }
+            else
+            {
+                // count is odd, return the middle element
+                return temp[count / 2];
+            }
+        }
     }
 
 
     class Studentai
     {
-
+        List<Stud> studentai;
 
         public Studentai()
+        {
+            studentai = new List<Stud>();
+        }
+
+        public void NuskaitytiIsKonsoles()
         {
 
         }
